c9e876d8cdd1200486e9880f4aff3eb16053e859
wowenstine <whowenstine@gmail.com>
2014-07-24 17:16:53 -0400

    Outbound Calls! -created home page with input form -added outbound call method to twilio controller -added bootstrap to layouts -made custom routes and methods -need to fix those to appease the RESTful gods

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,15 +1,41 @@
 class TwilioController < ApplicationController
-include Webhookable
+  include Webhookable
 
-after_filter :set_header
+  after_filter :set_header
 
-skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-	def voice 
-	  response = Twilio::TwiML::Response.new do |r|
-	    r.Play 'http://howenstine.co/rick_roll.mp3'
-	end
-	  render_twiml response
+  def voice
+    response = Twilio::TwiML::Response.new do |r|
+      r.Play 'http://howenstine.co/rick_roll.mp3'
+    end
+    render_twiml response
+  end
+
+  ACCOUNT_SID = ENV['account_sid']
+  ACCOUNT_TOKEN = ENV['auth_token']
+  CALLER_ID = '13095170892'
+
+  def makecall
+    if !params['number']
+      redirect_to :action => '.', 'msg' => 'Invalid phone number, ya big dummy!'
+      return
     end
 
+    # params sent to Twilio
+    data = {
+      :from => CALLER_ID,
+      :to => params['number'],
+      :url => 'http://howenstine.co/rick_roll.mp3',
+    }
+
+    begin
+      client = Twilio::REST::Client.new(ACCOUNT_SID, ACCOUNT_TOKEN)
+      client.account.calls.create data
+    rescue StandardError => bang
+      redirect_to :action => '.', 'msg' => "Error #{bang}"
+      return
+    end
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: "homes#show"
+
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   post 'twilio/voice' => 'twilio#voice'
+  post 'twilio/makecall' => 'twilio#makecall'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
